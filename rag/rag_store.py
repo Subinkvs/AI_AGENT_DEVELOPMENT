02c7dc8bb8bbb1c8b6868aa2a634b1019c38b2d6
Subinkvs <subinsundar9624@gmail.com>
2026-02-10 11:50:11 +0530

    minor updates

--- rag/rag_store.py
+++ rag/rag_store.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 EMBEDDINGS = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2")
